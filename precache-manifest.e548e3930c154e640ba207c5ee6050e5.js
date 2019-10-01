74323db33ff34d135817f9793c1fe4655ced7b0c
Aleksandr <sanyamaks14@gmail.com>
2019-10-01 23:44:05 +0400

    Updates

---
+++ precache-manifest.e548e3930c154e640ba207c5ee6050e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7afc690bc47c4f8baeff84200cb950f",
+    "url": "/test_task_login_react/index.html"
+  },
+  {
+    "revision": "bd76ed1f118d52c18390",
+    "url": "/test_task_login_react/static/css/main.e1a8ca02.chunk.css"
+  },
+  {
+    "revision": "174e4038292f1f710e4e",
+    "url": "/test_task_login_react/static/js/2.94718f50.chunk.js"
+  },
+  {
+    "revision": "bd76ed1f118d52c18390",
+    "url": "/test_task_login_react/static/js/main.8a8ee9d6.chunk.js"
+  },
+  {
+    "revision": "60f61132c8687d656355",
+    "url": "/test_task_login_react/static/js/runtime~main.e51aaea1.js"
+  },
+  {
+    "revision": "5a2b3893bd0ff4cb4e7c2e575e5e0c7c",
+    "url": "/test_task_login_react/static/media/Wallpaper.5a2b3893.png"
+  },
+  {
+    "revision": "6e5e5d97101a473649b66101ef3e52da",
+    "url": "/test_task_login_react/static/media/mercdev.6e5e5d97.svg"
+  }
+]);
